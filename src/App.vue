3de267b1f20c43190a39bfb9ec924e6e310cdc55
Vikram Vaswani <vikram@dagger.io>
2024-05-09 00:49:02 +0530

    Modify index page and assets
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <HelloWorld msg="Hello from Dagger!" />
     </div>
   </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,12 +6,7 @@ defineProps<{
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+    <h1>{{ msg }}</h1>
   </div>
 </template>
 
